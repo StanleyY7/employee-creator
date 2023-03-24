97da58a2cd851673318cfb157708674f4e1a4f51
Stanley <119549394+StanleyY7@users.noreply.github.com>
2023-03-24 20:16:11 +1100

    updating

--- production/src/main/java/employeecreator/production/posts/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package employeecreator.production.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
