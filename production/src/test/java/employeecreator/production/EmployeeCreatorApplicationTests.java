58d540d34a6fbbde3b2dc6345f20774c7fefe1eb
Stanley <119549394+StanleyY7@users.noreply.github.com>
2023-03-31 21:17:38 +1100

    updated tests

---
+++ production/src/test/java/employeecreator/production/EmployeeCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package employeecreator.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
