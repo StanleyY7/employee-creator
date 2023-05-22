d908b920d0dfcf6cea61fd47966fb1dcc549d486
StanleyY7 <stanleyyu7@gmail.com>
2023-05-23 09:46:55 +1000

    updated docker-compose and config

--- employee-creator-project/vite.config.ts
+++ employee-creator-project/vite.config.ts
@@ -4,17 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../production/src/main/resources/static",
-    emptyOutDir: true,
-  },
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: true,
+    strictPort: true,
+    port: 5173,
   },
 });
